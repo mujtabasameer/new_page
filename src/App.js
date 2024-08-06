740f9ee562e410a1331c2b1c0d7b881b21cb79b7
mujtabasameer <mujusameer22@gmail.com>
2024-08-06 12:14:13 +0500

    added new component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./Components/NavBar";
 import Hero from "./Components/Hero";
-
+import Analytics from "./Components/Analytics";
 
 function App() {
   return (
     <div>
        <Navbar/>
        <Hero/>
+       <Analytics/>
     </div>
   );
 }
